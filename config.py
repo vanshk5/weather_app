e09a60e09072864ba6a7f8934e2c41bb84e792be
vanshk5 <vanshkapoor.cu@gmail.com>
2025-08-27 22:04:36 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-MODEL_PATH = "model.joblib"
-TARGET_COL = "RainTomorrowMorning"
+# config.py
+API_KEY = "09d9e6f7a88e420297d11300252808"  # your WeatherAPI key
+MODEL_PATH = "model.joblib"                  # path to your trained model
+CITY = "Toronto,CA"
